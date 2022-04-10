47f2722d20c1e4586b0449b6e2aac8a3e963f083
James <jamesisom310@gmail.com>
2022-04-10 11:54:10 -0400

    Fixed connection issue

--- MemberForm.php
+++ MemberForm.php
@@ -2,22 +2,22 @@
 
 $fName = $_POST['fName'];
 $lName = $_POST['lName'];
-$phoneNum = $_POST['phoneNum'];
-$emailAdd = $_POST['emailAdd'];
-$discount = $_POST['discount'];
+$phoneNum = $_POST['phoneNumber'];
+$emailAdd = $_POST['emailAddress'];
+$discount = $_POST['discounts'];
 
-$servername = "us-cdbr-east-05.cleardb.net";
+$hostname = "us-cdbr-east-05.cleardb.net";
 $username = "b59b573e6c6aee";
 $password = "4b777f16";
 $db = "heroku_4859b9997f503e5";
 
-$conn = new mysqli_connect($servername, $username, $password, $db);
+$conn = new mysqli($hostname, $username, $password, $db);
 
 if ($conn->connect_error){
 	die("Connection failed: ". $conn->connect_error);
 }
 
-$sql = "insert into member(fName,lName,phoneNumber,emailAddress,discounts) values('$fName','$lName','$phoneNum','$emailAdd','$discount')";
+$sql = "insert into customer(fName,lName,phoneNumber,emailAddress,discounts) values('$fName','$lName','$phoneNum','$emailAdd','$discount')";
 
 if ($conn->query($sql) === TRUE) {
 	echo "ADDED: ".$fName.", ".$lName.", ".$phoneNum.", ".$emailAdd.", ".$discount;
